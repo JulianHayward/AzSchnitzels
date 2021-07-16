730a56d4e19f7e31fdc9bf98ac2cca2d54c6697c
quoteee <45695032+JulianHayward@users.noreply.github.com>
2021-07-16 13:33:42 +0200

    Create getAzCmdlet-APIEndpoint.ps1

---
+++ pwsh/getAzCmdlet-APIEndpoint.ps1
@@ -0,0 +1,5 @@
+$ra = Get-AzRoleAssignment -debug 5>&1
+($ra | Select-String -Pattern 'Absolute Uri:') | foreach-object {
+    [regex]$regex = '(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})'
+    [string]$_ | select-string -pattern $regex | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value } 
+}
